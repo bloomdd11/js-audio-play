03bbad868ae3d57fd10ebf5d0025470144e5b1c0
kaunghtet466 <kh685132@gmail.com>
2022-03-27 15:57:49 +0630

    algorithum changes

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ let next = document.querySelector('.next');
 let pre = document.querySelector('.pre');
 let playpause = document.querySelector('.playandpause');
 let loop = document.querySelector('.loop');
+let shuffle = document.querySelector('.shuffle');
 let demo = document.querySelector('#demo');
+
+let playRange = document.querySelector('form input[name="playRange"]');
+let playStart = document.querySelector('.playStart');
+let playEnd = document.querySelector('.playEnd');
+
+let volumeRange = document.querySelector('form input[name="volumeRange"]');
+let volumeDown = document.querySelector('.volumeDonw');
+let volumeUp = document.querySelector('.volumeUp');
 
 // set play
 let isPlaying = false;
 let audioID = 0;
 let loopID = 0;
-
+let playTime = 0;
+let shuffleOn = false;
+let shuffleArray = [];
 
 // set loop config
 let loopTrack = {
@@ -18,6 +29,10 @@ let loopTrack = {
     2: true // all song loop
 }
 
+// create audio element
+let audio = document.createElement('audio');
+audio.setAttribute('controls', false)
+demo.appendChild(audio);
 // set list of audio
 let audioTrack = {
     0: {
@@ -31,24 +46,42 @@ let audioTrack = {
     }
 };
 
-// create audio element
-let audio = document.createElement('audio');
-audio.setAttribute('controls', false)
-demo.appendChild(audio);
+function randomBgColor() {
+    // create random color
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    document.querySelector('body').style.backgroundColor = `rgb(${red},${green},${blue})`;
+}
+// set first song with random color
+audio.src = audioTrack[audioID]['path'];
+randomBgColor();
+audio.volume = volumeRange.value / 100;
 
+// event
 playpause.addEventListener('click', playTrack);
 next.addEventListener('click', nextAudio);
 pre.addEventListener('click', prevAudio);
 loop.addEventListener('click', loopAudio);
+shuffle.addEventListener('click', doShuffle);
+playRange.addEventListener('click', rangeChangeUpdate);
+volumeRange.addEventListener('click', volumeChangeUpdate);
 
-// play audio
+// set src for playTrack
+function setAudioSrc(id) {
+    audio.load();
+    audio.src = audioTrack[id]['path'];
+    playRange.value = 0;
+}
+
+// play and pause audio
 function playTrack() {
-    audio.src = audioTrack[audioID]['path'];
     if (!isPlaying) playAudio();
     else pauseAudio();
 }
 
 function playAudio() {
+    randomBgColor();
     audio.play();
     isPlaying = true;
     playpause.textContent = 'Play';
@@ -66,7 +99,13 @@ function nextAudio() {
     if (audioID >= Object.keys(audioTrack).length) {
         audioID = 0;
     }
-    audio.src = audioTrack[audioID]['path'];
+
+    // shuffle play
+    if (shuffleOn) {
+        setAudioSrc(shuffleArray[audioID]);
+    } else {
+        setAudioSrc(audioID);
+    }
     playAudio();
 }
 // prev song
@@ -75,7 +114,13 @@ function prevAudio() {
     if (audioID < 0) {
         audioID = Object.keys(audioTrack).length - 1;
     };
-    audio.src = audioTrack[audioID]['path'];
+
+    // shuffle play
+    if (shuffleOn) {
+        setAudioSrc(shuffleArray[audioID]);
+    } else {
+        setAudioSrc(audioID);
+    }
     playAudio();
 }
 
@@ -109,4 +154,88 @@ function loopOne() {
 // loop all
 function loopAll() {
     audio.addEventListener('ended', nextAudio)
+}
+
+// shuffle song
+function doShuffle() {
+    if (!shuffleOn) {
+        shuffle.textContent = 'Shuffle On';
+        shuffleOn = true;
+    } else {
+        shuffle.textContent = 'Shuffle Off';
+        shuffleOn = false;
+    }
+    playShuffleAudio();
+};
+
+function shuffleAudio() {
+    shuffleArray = [];
+    let copyArray = [...Object.keys(audioTrack)];
+    for (let i = 0; i < copyArray.length + 1; i++) {
+        let shuffleId = Math.floor(Math.random() * copyArray.length);
+        shuffleArray.push(copyArray[shuffleId]);
+        copyArray.splice(shuffleId, 1);
+    }
+    shuffleArray.push(copyArray[0]);
+    return shuffleArray;
+}
+
+function playShuffleAudio() {
+    shuffleAudio();
+    audio.addEventListener('ended', nextAudio);
+}
+
+// update current time
+let currentTimeUpdate = setInterval(() => {
+    // for duration time
+    let durationMinute = Math.floor(audio.duration / 60);
+    let durationSecond = Math.floor(audio.duration % 60);
+    if (durationSecond < 10) {
+        durationSecond = '0' + durationSecond;
+    }
+    if (durationMinute < 10) {
+        durationMinute = '0' + durationMinute;
+    }
+
+    // for current time
+    let currentSecond = Math.floor(audio.currentTime);
+    let currentMinute = 0;
+    if (currentSecond < 10) {
+        currentSecond = '0' + currentSecond;
+    }
+    if (currentMinute < 10) {
+        currentMinute = '0' + currentMinute;
+    }
+    if (currentSecond >= 60) {
+        currentSecond = Math.floor(audio.currentTime % 60);
+        if (currentSecond < 10) {
+            currentSecond = '0' + currentSecond;
+        }
+        currentMinute = Math.floor(audio.currentTime / 60);
+        if (currentMinute < 10) {
+            currentMinute = '0' + currentMinute;
+        }
+    }
+    // display time
+    playStart.textContent = currentMinute + ':' + currentSecond;
+    playEnd.textContent = durationMinute + ':' + durationSecond;
+}, 100)
+
+// range change update
+function rangeChangeUpdate() {
+    let rangeValue = playRange.value;
+    audio.currentTime = audio.duration * (rangeValue / 100);
+}
+
+// update range
+let updateRange = setInterval(() => {
+    i = 100 * (audio.currentTime / audio.duration)
+    playRange.value = i;
+}, 1000);
+
+// change volume range
+function volumeChangeUpdate() {
+    let volumeValue = volumeRange.value;
+    calculateVolume = 1 * (volumeValue / 100)
+    audio.volume = calculateVolume;
 }
